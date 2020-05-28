0a7b1ad31e6e26906bf0a742ae7683e62ded78ca
Youngmin Park <codepark2019@gmail.com>
2020-05-28 01:45:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,9 @@ def speak_to_grandma(string)
   if string == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif string != string.upcase
-    'HUH?! SPEAK UP, SONNY!''
+    'HUH?! SPEAK UP, SONNY!'
   else
-    'NO, NOT SINCE 1938!''
+    'NO, NOT SINCE 1938!'
   end
 end
 # Whatever you say to grandma, she should respond with
